6c55bd8071809fbc8cc61b002fb5d58dc95c6142
Randal T Meyer <randaltmeyer@Ladnar-iMac.local>
2017-07-25 12:39:01 -0700

    remove brain from filename

--- js/BattleHandHUD.js
+++ js/BattleHandHUD.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         BattleHand HUD
 // @namespace    http://brains.sth.ovh
-// @version      3.2.0
-// @description  BattleHand QoL HUD / Scout interface (sponsored by The βrain)
+// @version      3.2.1
+// @description  BattleHand QoL HUD / Scout interface (hosted by The βrain)
 // @author       Jai (feat. SyntheeR)
-// @updateURL    http://brains.sth.ovh/bh-hud/BattleHandBrainHUD.js
-// @require      http://brains.sth.ovh/bh-hud/3.2.0/default.min.js
+// @updateURL    http://brains.sth.ovh/bh-hud/BattleHandHUD.js
+// @require      http://brains.sth.ovh/bh-hud/3.2.1/default.min.js
 // @include      http://game261051.konggames.com/*
 // @include      http://www.kongregate.com/games/AnotherPlaceProd/*
 // @grant        unsafeWindow
